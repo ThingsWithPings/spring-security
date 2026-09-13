6de08a69d52d1ae2527ed3f279418fc124ebd6b9
ThingsWithPings <virajchamara0701@gmail.com>
2026-09-13 23:40:41 +0300

    disable default configuration for and add custom permit all and authentication

--- src/main/java/com/thingswithpings/controller/LoansController.java
+++ src/main/java/com/thingswithpings/controller/LoansController.java
@@ -4,10 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
+public class LoansController {
 
-    @GetMapping("/hello")
-    public String sayHello(){
-        return  "Hello";
+    @GetMapping("/myLoans")
+    public  String getLoansDetails () {
+        return "Here are the loans details from the DB";
     }
+
 }
