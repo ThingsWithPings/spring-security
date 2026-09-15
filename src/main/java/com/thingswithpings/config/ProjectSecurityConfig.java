7d86784eeac6d4b9365bc4196a0801d873ffd30c
ThingsWithPings <virajchamara0701@gmail.com>
2026-09-15 17:57:20 +0300

    use plain text passwords with {noop}

--- src/main/java/com/thingswithpings/config/ProjectSecurityConfig.java
+++ src/main/java/com/thingswithpings/config/ProjectSecurityConfig.java
@@ -3,6 +3,10 @@ package com.thingswithpings.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,8 +19,16 @@ public class ProjectSecurityConfig {
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount","/myBalance","/myCards").authenticated()
                 .requestMatchers("/notices","/contact","/error").permitAll());
-        http.formLogin(flc -> flc.disable());
+        http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}12345").authorities("read").build();
+
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
