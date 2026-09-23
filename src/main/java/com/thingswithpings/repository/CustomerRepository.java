e8f30cecb602ab42ab7b1535576c06e39afcd6d5
ThingsWithPings <virajchamara0701@gmail.com>
2026-09-23 23:08:46 +0300

    Add new service to implement user details service to load the user from custom user table

---
+++ src/main/java/com/thingswithpings/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.thingswithpings.repository;
+
+import com.thingswithpings.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
+    Optional<Customer> findByEmail(String email);
+}
